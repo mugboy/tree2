d7bac3f5695be486549373285fe947e60916335b
mugboy <110285156+mugboy@users.noreply.github.com>
2024-11-12 14:24:20 +0100

    testing github action YAYAYAYAYAYAYAYAYAY

--- tree2.py
+++ tree2.py
@@ -1,21 +1,25 @@
 from rich.console import Console
 from rich.tree import Tree
 from pathlib import Path
-from sys import argv
-
-path = Path(argv[1])
+import sys
 console = Console()
-for itemroot in path.glob("*"):
-    console.print(f"[green]{itemroot.relative_to(path)}")
 
-def make_tree(path: Path) -> Tree:
-    tree = Tree(f"[bold red]{path.name}")
+try:
+    path = Path(sys.argv[1])
     for itemroot in path.glob("*"):
-        if itemroot.is_dir():
-            tree.add(make_tree(itemroot))
-        else:
-            tree.add(f"[green]{itemroot.relative_to(path)}")
+        console.print(f"[green]{itemroot.relative_to(path)}")
 
-    return tree
+    def make_tree(path: Path) -> Tree:
+        tree = Tree(f"[bold red]{path.name}")
+        for itemroot in path.glob("*"):
+            if itemroot.is_dir():
+                tree.add(make_tree(itemroot))
+            else:
+                tree.add(f"[green]{itemroot.relative_to(path)}")
 
-console.print(make_tree(path))
+        return tree
+
+    console.print(make_tree(path))
+except IndexError:
+    console.print("[red]Directory name not given")
+    sys.exit(1)
